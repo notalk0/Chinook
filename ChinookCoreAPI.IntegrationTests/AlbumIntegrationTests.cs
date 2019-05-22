16584e805f046822a3a797e913528f24f9c9c00c
Reina Liew <notalk_0@hotmail.com>
2019-05-22 15:50:27 +0800

    Initial Commit

---
+++ ChinookCoreAPI.IntegrationTests/AlbumIntegrationTests.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ChinookCoreAPI.IntegrationTests {
+    public class AlbumIntegrationTests {
+        private readonly HttpClient _client;
+        private readonly TestServer _server;
+
+        public AlbumIntegrationTests() {
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            _server = new TestServer(new WebHostBuilder().UseConfiguration(configuration).UseStartup<Startup>());
+            _client = _server.CreateClient();
+        }
+
+        [Theory]
+        [InlineData("GET")]
+        public async Task TestAlbumAsync(string method) {
+            // Arrange
+            var request = new HttpRequestMessage(new HttpMethod(method), "/api/Albums");
+
+            // Act
+            var response = await _client.SendAsync(request);
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Theory]
+        [InlineData("GET")]
+        public async Task TestOneAlbumAsync(string method, int? id = null) {
+            // Arrange
+            var request = new HttpRequestMessage(new HttpMethod(method), $"/api/Albums/{id}");
+
+            // Act
+            var response = await _client.SendAsync(request);
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
